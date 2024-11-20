34c9e4b73f322efb9a67fc33f115cbd3abbe8fe2
Luis Fernando de Jesus Diaz Rojas <luisfernandogames2.0@gmail.com>
2024-11-19 19:55:55 -0500

    no se mando al campus

---
+++ laboratorio18.py
@@ -0,0 +1,53 @@
+#Crear Variables
+s = input("Ingrese una frase: ")
+i = input("Ingresa un numero entero: ")
+f = input("Ingresa un numero con decimales: ")
+b = input("Ingresa True or False: ")
+print("String = ",s)
+print("Int =",i)
+print("Float =",f)
+print("Boolean =",b)
+#Operaciones matematicas
+x = int(input("Ingresa un numero: "))
+y = int(input("Ingresa otro numero: "))
+print("Valor de X = ", x)
+print("Valor de Y = ", y)
+print("Operaciones")
+print("Suma = ", x + y)
+print("Resta = ", x - y)
+print("Multiplicacion = ", x * y)
+print("Division = ", x / y)
+print("Division Entera = ", x // y)
+print("Potencia = ", x ** y)
+print("Modulo = ", x % y)
+#Frases
+frase1 = input("Ingrese la Frase 1: ")
+frase2 = input("Ingresa la frase 2: ")
+frase = frase1 + " " + frase2
+print("La frase completa es: ",frase)
+#Listas
+numeros = [1, 2, 3, 4, 5]
+mixta = [3.14,"Python", True,5,6.3,False,"frase",8,"nueve",True]
+
+print("Lista de numeros =", numeros)
+print("Lista Mixta = ",mixta)
+print("Segundo elemento de la lista: ", mixta[1])
+mixta[2] = "false"
+print("Lista actualizada: ",mixta)
+
+#Diccionario
+persona = {
+    "nombre":"Luisillo",
+    "edad":23,
+    "ciudad":"Santa Marta",
+    "profesion":"estudiante"
+}
+print("Diccionario actualizado = ", persona)
+
+#Agregar Clave-Valor
+persona["hobby"] = "programar"
+print("Actualizar clave-valor =",persona)
+
+#Actualizar Clave-Valor
+persona["edad"] = 34
+print("Diccionario corregido =", persona)
---
+++ main.py
@@ -0,0 +1,39 @@
+x = 3
+y = 2
+print("Valor de X = ", x)
+print("Valor de Y = ", y)
+suma = x + y
+resta = x - y
+multiplicacion = x * y
+division = x / y
+division_entera = x // y
+potencia = x ** y
+modulo = x % y
+print(" ")
+print("Operaciones")
+print("Suma = ", suma)
+print("Resta = ", resta)
+print("Multiplicacion = ", multiplicacion)
+print("Division = ", division)
+print("Division Entera = ", division_entera)
+print("Potencia = ", potencia)
+print("Modulo = ", modulo)
+print(" ")
+print("X es igual a Y =", x==y)
+print("X es diferente de Y =", x!=y)
+print("X es mayor que Y = ", x>y)
+print("X es menor o igual a Y =", x<=y)
+print(" ")
+a = True
+b = False
+print("A and B = ", a and b)
+print("A or B = ", a or b)
+print("Not A = ", not a)
+print(" ")
+nombre = "Luisillo"
+saludo = "Hola ¿Quien es "
+mensaje = saludo + nombre + "?"
+print("Cadena de texto: ", nombre)
+print("Otra cadena: ",saludo)
+print("Concatenacion")
+print(mensaje)
